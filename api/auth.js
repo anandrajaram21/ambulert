783d9ca718dd532f8c8fbc5dc56559878232fcac
skizzle010 <raiutkarsh3219@gmail.com>
2023-01-13 17:17:34 +0530

    authentication with db

---
+++ api/auth.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const GOOGLE_CLIENT_ID = 'YOUR_GOOGLE_CLIENT_ID'
+const GOOGLE_CLIENT_SECRET = 'YOUR_GOOGLE_CLIENT_SECRET'
+
+
+passport.use(new GoogleStrategy({
+    clientID:     GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    })
+)
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
